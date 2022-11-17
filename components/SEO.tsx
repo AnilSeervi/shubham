84abf7c249eff5b24c43e7816d16f4f4195a3793
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2022-11-17 16:10:09 +0530

    initial setup

---
+++ components/SEO.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head"
+
+const SEO = () => {
+	return (
+		<Head>
+			<meta
+				name="viewport"
+				content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=yes"
+			/>
+			<link rel="shortcut icon" href="shubham.jpg" type="image/jpeg" />
+			<title>Shubham Srivastava | Zenduty</title>
+			<meta
+				name="description"
+				content="Hey! I'm Shubham and I head Developer Relations at Zenduty - an advanced incident management and response orchestration platform. Love talking about SRE, Observability and Product Design!"
+			/>
+			<meta name="robots" content="index,follow" />
+			<meta name="theme-color" content="#a033ff" />
+			<meta name="author" content="Shubham Srivastava" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@shhhhubham" />
+			<meta name="twitter:creator" content="@shhhhubham" />
+
+			<meta
+				property="og:description"
+				content="Hey! I'm Shubham and I head Developer Relations at Zenduty - an advanced incident management and response orchestration platform. Love talking about SRE, Observability and Product Design!"
+			/>
+			<meta
+				property="og:image"
+				content="https://shubham.pages.dev/assets/og-thumb.png"
+			/>
+			<meta property="og:image:alt" content="Shubham" />
+			<meta property="og:image:height" content="1080" />
+			<meta property="og:image:type" content="image/png" />
+			<meta property="og:image:width" content="1920" />
+			<meta property="og:site_name" content="Shubham Srivastava | Zenduty" />
+			<meta property="og:title" content="Shubham Srivastava" />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="https://shubham.pages.dev/" />
+		</Head>
+	)
+}
+
+export default SEO
